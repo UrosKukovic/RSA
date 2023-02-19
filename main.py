96f9b380db84a13d4b0cfed8c6c780102e844e2d
Uroš Kukovič <uros.kukovic1@student.um.si>
2023-02-19 21:04:33 +0100

    Removed checks

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ t = 0
 
 
 def primes_in_range():
-<<<<<<< HEAD
     min = 15000
     max = 25000
 
@@ -72,64 +71,3 @@ def two_random_primes():
 primes_in_range()
 
 two_random_primes()
-=======
-    min = 5903
-    max = 10000
-
-    for i in range(min, max):
-        for x in range(2, i):
-            if (i % x == 0):
-                break
-        else:
-            primes.append(i)
-
-
-def two_random_primes():
-
-    while True:
-        x = random.randint(0, (len(primes))-1)
-        y = random.randint(0, (len(primes))-1)
-        p = primes[x]
-        q = primes[y]
-        if p != q:
-            break
-
-    print(p, q)
-    if p == q:
-        sys.exit()
-
-
-#print("Izberite moznost:\n")
-# print("1-Kriptiranje\n")
-# print("2-Dekriptiranje\n")
-#choice = int(input())
-# if choice == 1:
-#    print("\nVnesite sporocilo: \n")
-#    in_message = input()
-#
-# elif choice == 2:
-#    print("\nVnesite sporocilo: \n")
-#    in_message = input().split()
-#
-# else:
-#    print("Nepravilno!")
-#
-# if choice == 1:
-#    for i in in_message:
-#        out_message.append(ord(i))
-#
-#    for x in out_message:
-#        print(x*key)
-#
-# elif choice == 2:
-#    for i in in_message:
-#        rez = int(i)/key
-#        out_message.append(rez)
-#
-#    for i in out_message:
-#        print(chr(int(i)))
-#
-primes_in_range()
-while True:
-    two_random_primes()
->>>>>>> generate_keys
